bcea1600c0c40eee78422c3ce8a4840db2b2be54
Steven Craeye <42608032+stevencraeye@users.noreply.github.com>
2025-03-20 10:01:58 +0200

    feat: add tables to enable groups feature (#3)
    Co-authored-by: Steven Craeye <stevencraeye@Stevens-MacBook-Pro.local>

---
+++ database/sql/tables/group_members.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS group_members (
+    id BIGSERIAL PRIMARY KEY,
+    group_id BIGINT,
+    user_id BIGINT,
+    role VARCHAR,
+    joined_at TIMESTAMPZ,
+    CONSTRAINT fk_group_members_group FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE,
+    CONSTRAINT fk_group_members_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
