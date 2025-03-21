142cda6b07aa18a414b1e0e06947d88ea8f8ed9e
Steven Craeye <stevencraeye@Stevens-MacBook-Pro.local>
2025-03-21 10:51:31 +0200

    feat: add dao methods

--- backend/daos/groupsDao.go
+++ backend/daos/groupsDao.go
@@ -9,16 +9,16 @@ import (
 type GroupsDaoInterface interface {
 	CreateGroup(group models.Group) error
 	UpdateGroup(group models.Group) error
-	DeleteGroup(groupId int64) error // todo
+	DeleteGroup(groupID int64) error
 	CreateGroupMember(member models.GroupMember) error
-	UpdateGroupMember(member models.GroupMember) error // todo
-	DeleteGroupMember(userId int64) error              // todo
-	CreateGroupGoal(goal models.GroupGoal) error       // todo
-	UpdateGroupGoal(goal models.GroupGoal) error       // todo
-	DeleteGroupGoal(goalId int64) error                // todo
+	UpdateGroupMember(member models.GroupMember) error
+	DeleteGroupMember(userID int64) error        // todo
+	CreateGroupGoal(goal models.GroupGoal) error // todo
+	UpdateGroupGoal(goal models.GroupGoal) error // todo
+	DeleteGroupGoal(goalID int64) error          // todo
 
-	GetUserGroups(userId int64) ([]models.Group, error)          // todo
-	GetGroupMembers(groupId int64) ([]models.GroupMember, error) // todo
+	GetUserGroups(userID int64) ([]models.Group, error)          // todo
+	GetGroupMembers(groupID int64) ([]models.GroupMember, error) // todo
 }
 
 type GroupsDao struct {
@@ -67,6 +67,20 @@ func (dao *GroupsDao) UpdateGroup(group models.Group) error {
 	return nil
 }
 
+func (dao *GroupsDao) DeleteGroup(groupID int64) error {
+	sql := `
+		DELETE FROM
+			groups
+		WHERE id = $1;
+	`
+	_, err := dao.db.Exec(sql, groupID)
+	if err != nil {
+		dao.l.Printf("Error deleting group: %v", err)
+		return err
+	}
+	return nil
+}
+
 func (dao *GroupsDao) CreateGroupMember(member models.GroupMember) error {
 	sql := `
 		INSERT INTO group_members (
@@ -81,6 +95,22 @@ func (dao *GroupsDao) CreateGroupMember(member models.GroupMember) error {
 	_, err := dao.db.Exec(sql, member.GroupId, member.UserId, member.Role, member.JoinedAt)
 	if err != nil {
 		dao.l.Printf("Error adding group member: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (dao *GroupsDao) UpdateGroupMember(member models.GroupMember) error {
+	sql := `
+		UPDATE
+			group_members
+		SET
+			role = $2
+		WHERE id = $1;
+	`
+	_, err := dao.db.Exec(sql, member.ID, member.Role)
+	if err != nil {
+		dao.l.Printf("Error updating group member: %v", err)
 		return err
 	}
 	return nil
